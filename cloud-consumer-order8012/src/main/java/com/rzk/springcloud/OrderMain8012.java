6eccd61d3198cb4b1fec5dc46cc084079778300b
Rzk <1769073060@qq.com>
2022-07-03 21:21:13 +0800

    add:添加订单模块

---
+++ cloud-consumer-order8012/src/main/java/com/rzk/springcloud/OrderMain8012.java
@@ -0,0 +1,12 @@
+package com.rzk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMain8012 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain8012.class,args);
+    }
+}
