3c7d29c07142cfdadca4fea0c47930402ac9690f
Rzk <1769073060@qq.com>
2022-07-03 16:17:15 +0800

    add:添加代码生成器

---
+++ cloud-generator/src/main/java/com/rzk/test1/MySQLGeneratorTest.java
@@ -0,0 +1,50 @@
+package com.rzk.test1;
+
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * MySQL 代码生成
+ *
+ * @author lanjerry
+ * @since 3.5.3
+ */
+public class MySQLGeneratorTest extends BaseGeneratorTest {
+
+    /**
+     * 数据源配置
+     */
+    private static final DataSourceConfig DATA_SOURCE_CONFIG = new DataSourceConfig
+            .Builder("jdbc:mysql://" +
+            "3310/cloud2021?characterEncoding=utf8&serverTimezone=UTC","root","rzkruizhukai123.")
+            .schema("rzk")
+            .build();
+
+
+    public static void main(String[] args) {
+        String projectPath = System.getProperty("user.dir");
+
+        AutoGenerator generator = new AutoGenerator(DATA_SOURCE_CONFIG);
+        generator.strategy(strategyConfig().build());
+
+        generator.global(globalConfig().build());
+
+        // 设置自定义路径
+        Map<OutputFile, String> pathInfo = new HashMap<>();
+        pathInfo.put(OutputFile.xml, projectPath+"/src/main/java/xml");
+        pathInfo.put(OutputFile.entity, projectPath+"/src/main/java/pojo");
+        pathInfo.put(OutputFile.mapper, projectPath+"/src/main/java/mapper");
+        pathInfo.put(OutputFile.service, projectPath+"/src/main/java/service");
+        pathInfo.put(OutputFile.serviceImpl, projectPath+"/src/main/java/service/Impl");
+        pathInfo.put(OutputFile.controller, projectPath+"/src/main/java/controller");
+        generator.packageInfo(packageConfig().pathInfo(pathInfo).build());
+
+        generator.execute();
+    }
+
+}
---
+++ cloud-generator/src/main/java/com/rzk/test3/NewCodeGenerator.java
@@ -0,0 +1,80 @@
+package com.rzk.test3;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.fill.Column;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class NewCodeGenerator {
+
+    public static void main(String[] args) {
+
+//    new DataSourceConfig.Builder("jdbc:mysql://120.79.7.36:3310/cloud2021?characterEncoding=utf8&serverTimezone=UTC","root","rzkruizhukai123.")
+//            .build();
+        String projectPath = System.getProperty("user.dir");
+    FastAutoGenerator.create("jdbc:mysql:///cloud2021?characterEncoding=utf8&serverTimezone=UTC","root","rzkruizhukai123.")
+            .globalConfig(builder -> {
+        builder.author("rzk") // 设置作者
+                .enableSwagger() // 开启 swagger 模式
+                .fileOverride() // 覆盖已生成文件
+                .outputDir(projectPath+"/src/main/java"); // 指定输出目录
+    })
+            .packageConfig(builder -> {
+        builder.parent("com.rzk") // 设置父包名
+                .moduleName("springcloud") // 设置父包模块名
+                .pathInfo(Collections.singletonMap(OutputFile.xml, "/src/main/resources/com/rzk/mapper/")); // 设置mapperXml生成路径
+    })
+            .strategyConfig((scanner, builder)  -> {
+        builder.addInclude(scanner.apply("表名，多个英文逗号分割").split(",")) // 设置需要生成的表名
+                .addTablePrefix("t_", "c_")
+                .controllerBuilder().enableFileOverride()//开启重新生成覆盖  官方源码说fileOverride后续会做删除 ,后续用这个替换enableFileOverride
+                .serviceBuilder().enableFileOverride()//开启重新生成覆盖
+                .entityBuilder().enableFileOverride()//开启重新生成覆盖
+                .mapperBuilder().enableFileOverride()//开启重新生成覆盖
+        ; // 设置过滤表前缀
+    })
+            .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+            .execute();
+    }
+
+
+/**
+    FastAutoGenerator.create("jdbc:mysql://120.79.7.36:3310/cloud2021?characterEncoding=utf8&serverTimezone=UTC","root","rzkruizhukai123.")
+            // 全局配置
+            .globalConfig((scanner, builder) -> builder.author(scanner.apply("请输入作者名称？")).fileOverride())
+            // 包配置
+            .packageConfig((scanner, builder) -> builder.parent(scanner.apply("请输入包名？")))
+            // 策略配置
+            .strategyConfig((scanner, builder) -> builder.addInclude(getTables(scanner.apply("请输入表名，多个英文逗号分隔？所有输入 all")))
+            .controllerBuilder().enableRestStyle().enableHyphenStyle()
+                        .entityBuilder().enableLombok().addTableFills(
+                                new Column("create_time", FieldFill.INSERT)
+                        ).build())
+
+        模板引擎配置，默认 Velocity 可选模板引擎 Beetl 或 Freemarker
+       .templateEngine(new BeetlTemplateEngine())
+       .templateEngine(new FreemarkerTemplateEngine())
+
+            .execute();
+
+
+
+    }
+
+
+    // 处理 all 情况
+    protected static List<String> getTables(String tables) {
+        return "all".equals(tables) ? Collections.emptyList() : Arrays.asList(tables.split(","));
+    }
+    **/
+
+}
