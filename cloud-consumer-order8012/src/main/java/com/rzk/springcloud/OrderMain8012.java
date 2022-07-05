073656d38a2a6210b34553b6c39677321ecd1c4a
Rzk <1769073060@qq.com>
2022-07-05 15:31:24 +0800

    update:添加Eureka服务

--- cloud-consumer-order8012/src/main/java/com/rzk/springcloud/OrderMain8012.java
+++ cloud-consumer-order8012/src/main/java/com/rzk/springcloud/OrderMain8012.java
@@ -2,7 +2,9 @@ package com.rzk.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient /*eureka注册中心的客户端*/
 @SpringBootApplication
 public class OrderMain8012 {
 
